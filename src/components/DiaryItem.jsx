b7125560b874a142088a55856c8a57147045254e
gilhwan <dyghks123@gmail.com>
2022-07-27 16:08:38 +0900

    🚧  Addto UI components

---
+++ src/components/DiaryItem.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const DiaryItem = () => {
+  return <div>다이어리 아이템</div>;
+};
+
+export default DiaryItem;
---
+++ src/components/DiaryList.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const DiaryList = () => {
+  return <div> 다이어리 리스트</div>;
+};
+
+export default DiaryList;
