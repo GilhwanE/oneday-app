a9b3405d811609f3f46c4f6021c0abc720924605
gilhwan <dyghks123@gmail.com>
2022-08-08 22:45:29 +0900

    💄 UI styled-components

--- src/components/InputPage.jsx
+++ src/components/InputPage.jsx
@@ -1,6 +1,23 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
+import styled from 'styled-components';
 
-const InputPage = () => {
+const Input = styled.input`
+  height: 30px;
+  margin-bottom: 30px;
+  font-size: 1rem;
+  border: 1px solid gray;
+`;
+
+const TextArea = styled.textarea`
+  width: 100%;
+  max-width: 300px;
+  border: 1px solid gray;
+`;
+
+const InputPage = ({ Container }) => {
+  const reftitle = useRef();
+  const refcontent = useRef();
+
   const [state, setState] = useState({
     title: '',
     content: '',
@@ -11,26 +28,54 @@ const InputPage = () => {
       ...state,
       [e.target.name]: e.target.value, // 객체 프로퍼티를 접근하기 위한 괄호표기법 ()
     }); // 이벤트(onchange)가 발생되었을때 현재 입력되는 값으로 변경 시켜라
+
+    console.log(e.target.value);
+  };
+
+  const handleSumbit = () => {
+    if (state.title.length < 5) {
+      reftitle.current.focus();
+      alert('제목은 5글자 이상입니다.');
+    }
+
+    if (state.content.length < 5) {
+      refcontent.current.focus();
+      alert(' 5글자 입력이상을 입력해주세요');
+    }
   };
 
   return (
     <>
-      <input
-        type="text"
-        name="title"
-        value={state.title}
-        onChange={HandleInputText}
-      />
-      <textarea
-        type="text"
-        name="content"
-        value={state.content}
-        onChange={HandleInputText}
-      />
+      <Container>
+        <Input
+          ref={reftitle}
+          type="text"
+          name="title"
+          value={state.title}
+          placeholder="제목을 입력해주세요"
+          onChange={HandleInputText}
+        />
+        <TextArea
+          ref={refcontent}
+          type="text"
+          name="content"
+          value={state.content}
+          onChange={HandleInputText}
+        />
 
-      <div>
-        <button>저장하기</button>
-      </div>
+        <div>
+          <span>오늘 만족도 :</span>
+          <select name="score" onChange={HandleInputText}>
+            <option value="별로">별로</option>
+            <option value="보통">보통</option>
+            <option value="만족">만족</option>
+          </select>
+        </div>
+
+        <div>
+          <button onClick={handleSumbit}>저장하기</button>
+        </div>
+      </Container>
     </>
   );
 };
