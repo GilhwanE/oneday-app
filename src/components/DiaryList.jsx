5c94e2a43260098f986f0b400c98a2efc8b93c67
gilhwan <dyghks123@gmail.com>
2022-08-08 22:45:00 +0900

    🚧 Add to DiaryList components

--- src/components/DiaryList.jsx
+++ src/components/DiaryList.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import DiaryItem from './DiaryItem';
 
-const DiaryList = () => {
-  return <div> 다이어리 리스트</div>;
+const DiaryList = ({ diaryList }) => {
+  return (
+    <>
+      <h2> 다이어리 리스트는 {diaryList.length}개가 있습니다.</h2>
+      <div>
+        {diaryList.map((it) => (
+          <DiaryItem key={it.id} {...it} />
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default DiaryList;
